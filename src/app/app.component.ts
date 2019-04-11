a3c1e8b9210d153b171db392570705f1bde60b39
gellert.solyom <gellert.solyom@pitechnologies.ro>
2019-04-11 16:57:20 +0300

    feat: generated angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msct-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'meme-sanctuary';
+}
