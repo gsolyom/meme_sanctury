f6e21269f5081e2795e325b84e65879b429f530f
gellert.solyom <gellert.solyom@pitechnologies.ro>
2019-04-12 14:52:27 +0300

    refactor(app): add core module

--- src/app/core/components/root/app.component.ts
+++ src/app/core/components/root/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meme-sanctuary';
+  title = 'Meme Sanctuary';
 }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/root/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AppComponent
+  ]
+})
+export class CoreModule { }
